16a93e027fed175d1f376d0ddd4d308c1886b971
Bernd Engels <engels@goldenacker.de>
2020-05-28 16:14:33 +0200

    stuff

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,37 +1,66 @@
 <template>
     <div class="hello">
-        <h1>{{ title }}</h1>
+        <!-- AddTodo Komponente  -->
+        <form @submit.prevent="false">
+            <input type="text" name="title" v-model="title">
+            <b-button @click="addTodo" class="btn-sm btn-indo float-right">Add Todo</b-button>
+        </form>
+
         <ul>
+            <!-- Todo Komponente  -->
             <li v-for="todo in todos" :key="todo.id">
-                <input type="checkbox" name="done" class="">
-                <span>{{ todo.title }}</span>
-                <b-button class="btn-sm btn-danger float-right">löschen</b-button>
+                <form @submit.prevent="false">
+                    <input type="checkbox" name="done" v-model="todo.done">
+                    <span :class="{done: todo.done}">{{ todo.title }}</span>
+                    <b-button @click="deleteTodo(todo)" class="btn-sm btn-danger float-right">löschen</b-button>
+                </form>
             </li>
         </ul>
     </div>
 </template>
 
 <script>
-    // eslint-disable-next-line no-unused-vars
     import todoData from "../store/todos.json"
-
 	export default {
 		name: 'Todos',
-		props: ['title'],
 		data() {
 			return {
 				todos: todoData,
+				title: null,
 			}
-		}
+		},
+		methods: {
+			addTodo() {
+				var newData  = {
+					id: this.getLastId(),
+                    title: this.title,
+                    done: false
+                }
+				console.log(newData)
+                this.todos.push(newData)
+            },
+            deleteTodo(todo) {
+				console.info(todo)
+//                this.todos = this.todos.filter(t => t !== todo);
+                this.todos = this.todos.filter(function(t){
+                    if(t !== todo) {
+						return t;
+                    }
+                });
+            },
+            getLastId() {
+				return this.todos.map(t => t.id).sort().pop() + 1
+            }
+        }
 	}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
     .done {
-
+        text-decoration: line-through;
     }
     ul {
+        width: 80%;
         list-style: none;
         list-style-type: none;
         padding: 0;
@@ -39,7 +68,8 @@
     li {
         list-style: none;
         height: 2.0rem;
-        margin: 0 10px;
+        line-height: 2.0rem;
+        margin: 5px 10px 0 10px;
         border: 1px solid #42b983;
         border-radius: 5px;
         text-align: left;
